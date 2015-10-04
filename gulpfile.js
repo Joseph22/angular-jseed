cf5b17453834946bf47d433c270937eefdc3a07c
Joseph Perez <joseph.ani22@gmail.com>
2015-10-03 21:34:12 -0430

    add gulp-sass tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var browserify = require('browserify'),
     source = require('vinyl-source-stream'),
     connect = require('gulp-connect'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
     gulp = require('gulp');
 
 var liveReload = false;
@@ -11,13 +13,27 @@ gulp.task('browserify', function() {
   return browserify('./app/js/app.js',{ debug: true})
   .bundle()
   .pipe(source('app.js'))
-  .pipe(gulp.dest('./app/public/'))
+  .pipe(gulp.dest('./app/public/dist/js'))
   //.pipe(connect.reload());
 });
 
-gulp.task('server',['browserify'],function(){
+gulp.task('server',['browserify','sass-debug'],function(){
   connect.server({
     root: 'app/public',
     livereload: liveReload
   });
+});
+
+gulp.task('sass-debug',function(){
+  gulp.src('./app/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error',sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./app/public/dist/css'));
+});
+
+gulp.task('sass',function(){
+  gulp.src('./app/sass/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error',sass.logError))
+    .pipe(gulp.dest('./app/public/dist/css'));
 });
