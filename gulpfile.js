8107d3c11184f8b1a1a9759c78126e2a341f0b3c
Joseph Perez <joseph.ani22@gmail.com>
2015-10-04 23:33:21 -0430

    add tasks gulp: build:debug, build:debug:all & watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,139 @@
 'use strict';
 
-var path = require('path');
-
 var browserify = require('browserify'),
+    del = require('del'),
+    path = require('path'),
     source = require('vinyl-source-stream'),
     connect = require('gulp-connect'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
-    concatCss = require('gulp-concat-css'),
+    concat = require('gulp-concat'),
     file = require('gulp-file'),
+    minifyCss = require('gulp-minify-css'),
     gulp = require('gulp');
     
-var liveReload = false;
-var cssFiles = ['./vendor/bootstrap/dist/css/bootstrap.css',
-                './vendor/bootstrap/dist/css/bootstrap-theme.css',
-                './app/dist-css/styles.css'];
+var liveReload = true,
+    
+    cssVendor = ['./vendor/bootstrap/dist/css/bootstrap.css',
+                './vendor/bootstrap/dist/css/bootstrap-theme.css'],
 
-gulp.task('browserify', function() {
-  return browserify('./app/js/app.js',{ debug: true})
-  .bundle()
-  .pipe(source('app.js'))
-  .pipe(gulp.dest('./app/public/dist/js'))
-  //.pipe(connect.reload());
-});
+    cssApp = ['./app/dist-css/styles.css'],
 
-gulp.task('server',['browserify','sass-debug'],function(){
-  connect.server({
-    root: 'app/public',
-    livereload: liveReload
-  });
-});
+    cssFiles = cssVendor.concat(cssApp),
 
-gulp.task('sass-debug',function(){
-  gulp.src('./app/sass/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error',sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./app/dist-css'));
-});
+    fonts = ['./vendor/bootstrap/dist/fonts/**/*'],
 
-gulp.task('sass',function(){
-  gulp.src('./app/sass/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error',sass.logError))
-    .pipe(gulp.dest('./app/dist-css'));
-});
+    delStyles = ['./app/public/dist/css'],
 
-gulp.task('styles-debug',['sass-debug'],function(){
-  
+    delFonts = ['./app/public/dist/fonts'],
+
+    delCss = ['./app/dist-css'];
+
+
+function genAppCss()
+{
   var str = '';
   
   for(var i=0;i<cssFiles.length;i++){
     str += '@import url(\'./' + path.basename(cssFiles[i]) + '\');\n';
   }
   
-  gulp.src(cssFiles)
-    .pipe(file('app.css',str))
-    .pipe(gulp.dest('./app/public/dist/css'));
-  
-  gulp.src('./vendor/bootstrap/dist/fonts/**/*')
-    .pipe(gulp.dest('./app/public/dist/fonts')); 
+  return str;
+}
+
+//gulp Tasks
+
+gulp.task('browserify', function() {
+  return browserify('./app/js/app.js',{ debug: true})
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./app/public/dist/js'))
+  .pipe(connect.reload());
 });
 
+gulp.task('clean:styles',function(){
+   return del(delStyles);
+});
 
+gulp.task('clean:sass',function(){
+   return del(delCss);
+});
 
+gulp.task('clean:fonts',function(){
+   return del(delFonts);
+});
+
+gulp.task('sass:debug',['clean:sass'],function(){
+  return gulp.src('./app/sass/**/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error',sass.logError))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./app/dist-css'));    
+});
+
+gulp.task('sass',['clean:sass'],function(){
+  return gulp.src('./app/sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error',sass.logError))
+        .pipe(gulp.dest('./app/dist-css'));  
+  
+});
+
+gulp.task('fonts',['clean:fonts'],function(){
+  return gulp.src(fonts)
+            .pipe(gulp.dest('./app/public/dist/fonts'));
+});
+
+gulp.task('styles:debug:all',['sass:debug',
+                          'clean:styles',
+                          'fonts'],function(){
+  
+  return gulp.src(cssFiles)
+      .pipe(file('app.css',genAppCss()))
+      .pipe(gulp.dest('./app/public/dist/css'));
+  
+});
+
+gulp.task('styles:debug',['sass:debug'],function(){
+  
+  return gulp.src(cssApp)
+      .pipe(file('app.css',genAppCss()))
+      .pipe(gulp.dest('./app/public/dist/css'))
+      .pipe(connect.reload());
+});
+
+gulp.task('styles',['sass',
+                    'clean:styles',
+                    'fonts'],function(){
+  
+  return gulp.src(cssFiles)
+        .pipe(minifyCss())
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest('./app/public/dist/css'));
+});
+
+gulp.task('build:debug:all',['browserify','styles:debug:all']);
+gulp.task('build:debug',['browserify','styles:debug']);
+
+gulp.task('server',['build:debug:all'],function(){
+  connect.server({
+    root: 'app/public',
+    livereload: liveReload
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.start('server');
+
+  gulp.watch([
+    'app/js/**/*.js'
+    //,'test/**/*.js'
+  ], ['browserify']);
+
+  gulp.watch([
+    'app/sass/**/*.scss'
+  ], ['styles:debug']);
+
+//  gulp.watch(['app/**/*.html',
+//           '!app/js/vendor/**/*.html'], ['reload']);
+
+});
